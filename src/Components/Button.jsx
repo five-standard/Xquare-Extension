c2913df267ec50fafbbd52a50665f1c2921c54f5
six-standard <dbrrl1127@gmail.com>
2023-10-29 19:25:44 +0900

    🌧 :: 기본 컴포넌트 제작

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,24 +1,25 @@
 import { styled } from 'styled-components';
 
-export const Button = ({ text, action, width, height }) => {  
-  return <Content onClick={action} width={width ? width : "250px"} height={height ? height : "40px"}>
+export const Button = ({ text, action, width, height, disabled }) => {  
+  return <Content onClick={action} width={width} height={height} disabled={disabled}>
     {text}
   </Content>
 }
 
-const Content = styled.div`
+const Content = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 10px;
   padding: 10px;
   box-sizing: border-box;
-  font-size: 10px;
-  background: #9550F9;
+  font-size: 12px;
+  background: ${({disabled}) => disabled ? "#EADCFF" : "#9550F9"};
   font-weight: 400;
   color: #ffffff;
-  cursor: pointer;
-  width: ${(width) => width};
+  cursor: ${({disabled}) => disabled ? "not-allowed" : "pointer"};
+  width: ${({width}) => width ? width : "250px"};
+  height: ${({height}) => height ? height : "40px"};
   &:active {
     background: #EADCFF;
   }
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,30 +1,28 @@
 import { styled } from 'styled-components';
 
-export const Input = ({ type, placeholder, value, action, width, height }) => {
-  return <Content placeholder={placeholder} type={type} value={value} action={action} width={width} height={height} />
+export const Input = ({ type, placeholder, value, action, width, height, id }) => {
+  return <Content placeholder={placeholder} type={type} value={value} onChange={action} width={width} height={height} id={id} />
 }
-
-Input.defaultProps = {
-  type: "text",
-  width: "280px",
-  height: "35px"		
-};
 
 const Content = styled.input`
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 10px;
+  border-radius: 5px;
   padding: 10px;
   box-sizing: border-box;
   font-size: 12px;
-  border: 1px solid #a7a7a7;
+  border: 1px solid ${({value}) => value==="" ? "#C0BFC4" : "#a7a7a7"};
   font-weight: 500;
   color: #000000;
+  background: ${({value}) => value==="" ? "#F9F7FA" : "#ffffff"};
   cursor: text;
-  width: ${(width) => width};
-  height: ${(height) => height};
+  width: ${({width}) => width ? width : "280px"};
+  height: ${({height}) => height ? height : "35px"};
   &:focus {
-    border: 1px solid #5C5960;
+    border: 1px solid ${({value}) => value==="" ? "#C0BFC4" : "#5C5960"};
+  }
+  &::placeholder {
+    color: #C0BFC4;
   }
 `
