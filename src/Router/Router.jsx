b6a25ad2136d312eb422cd540e6917e18930da8b
six-standard <dbrrl1127@gmail.com>
2023-10-29 19:27:51 +0900

    🌧 :: 라우트 추가 회원가입, 홈 추가 accessToken 가져오기 추가

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,12 +1,28 @@
+/*global chrome*/
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 import { Main } from "../Pages/Main";
 import { Login } from "../Pages/Login";
+import { SignUp } from "../Pages/SignUp";
+import { Home } from "../Pages/Home";
+import { token } from "../Utils/Atoms";
+import { useRecoilState } from "recoil";
 
 export const Router = () => {
+  const [accessToken, setAccessToken] = useRecoilState(token);
+  chrome.cookies.get({url: "http://localhost:3000", name: "accessToken"}).then(res => res && setAccessToken(res.value));
+  
   return <MemoryRouter>
     <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
+      { !accessToken
+        ? <>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </>
+        : <>
+        <Route path="/" element={<Home />} />
+        </>
+      }
     </Routes>
   </MemoryRouter>
 }
