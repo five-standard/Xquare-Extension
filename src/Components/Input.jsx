3f01b2b864e7299f7de34587c6f1747e7226fa22
six-standard <dbrrl1127@gmail.com>
2023-11-02 23:37:43 +0900

    🌧 :: 로그인 컴포넌트 완성

---
+++ src/Components/Input.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const Input = ({ type, placeholder, value, action, id }) => {
+  return <Content type={type} placeholder={placeholder} onChange={action} value={value} id={id} />
+}
+
+const Content = styled.input`
+  width: 80%;
+  height: 40px;
+  padding: 10px;
+  box-sizing: border-box;
+  background: ${({value}) => value !== "" ? "#FFFFFF": "#bdbbbb"};
+  border: 1px solid ${({value}) => value !== "" ? "#000000" : "#635d63"};
+  border-radius: 15px;
+  &::placeholder {
+    color: #605f66;
+  }
+`
