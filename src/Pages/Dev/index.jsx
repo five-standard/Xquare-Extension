9b928db8551aee20994db76af9e2cbc78f8f1c02
six-standard <dbrrl1127@gmail.com>
2023-11-03 20:25:23 +0900

    🐛 :: 제작중 페이지 오류 수정 화면 크기가 늘어나는 오류 수정

--- src/Pages/Dev/index.jsx
+++ src/Pages/Dev/index.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Dev = () => {
   return <Wrapper>
     <h1>아직 개발중인 기능입니다.</h1>
-    <h1>불편 사항은 아래 이메일에 보내주세요</h1>
+    <h1>불편 사항은 아래 이메일로 보내주세요</h1>
     <h1>dbrrlwns1127@dsm.hs.kr</h1>
   </Wrapper>
 }
@@ -14,7 +14,7 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   width: 100%;
-  height: 450px;
+  height: 400px;
   & > h1 {
     font-size: 15px;
     font-weight: 500;
