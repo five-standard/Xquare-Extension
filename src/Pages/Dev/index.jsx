305b32ac63b17808d6ac517249769a9c7c415f09
six-standard <dbrrl1127@gmail.com>
2023-11-03 20:02:13 +0900

    🌧 :: 제작중 페이지 추가

---
+++ src/Pages/Dev/index.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Dev = () => {
+  return <Wrapper>
+    <h1>아직 개발중인 기능입니다.</h1>
+    <h1>불편 사항은 아래 이메일에 보내주세요</h1>
+    <h1>dbrrlwns1127@dsm.hs.kr</h1>
+  </Wrapper>
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 450px;
+  & > h1 {
+    font-size: 15px;
+    font-weight: 500;
+  }
+`
