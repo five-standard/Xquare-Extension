4668a9c551b0d48c3862a210ebf8f0690c269254
six-standard <dbrrl1127@gmail.com>
2023-11-03 20:01:08 +0900

    🌧 :: 인증 API 업데이트 로그아웃, 토큰 리프레쉬 API 추가

--- src/Api/Auth/index.js
+++ src/Api/Auth/index.js
@@ -3,3 +3,15 @@ import { instance } from "../axios"
 export const postLogin = async (data) => {
   return await instance.post("/users/login", data);
 }
+
+export const postLogout = async (token) => {
+  return await instance.put("/users", {}, { headers: {
+    Authorization: `Bearer ${token}`
+  } })
+}
+
+export const postRefresh = async (token) => {
+  return await instance.put("/users/login", {}, { headers: {
+    "Refresh-Token": `Bearer ${token}`
+  }})
+}
