8598e9cbae8a726c6d28230775b57be41cc91b5b
six-standard <dbrrl1127@gmail.com>
2023-11-03 20:04:57 +0900

    🌧 :: 라우터 업데이트 refreshToken 추가 및 코드 정리

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,25 +1,31 @@
 /*global chrome*/
 import { MemoryRouter, Routes, Route } from "react-router-dom";
-import { Home } from "../Pages/Home";
-import { Layout } from "./Layout";
-import { TimeTable } from "../Pages/TimeTable";
-import { Apply } from "../Pages/Apply";
-import { token } from "../Utils/Atoms";
 import { useSetRecoilState } from "recoil";
+import { refresh, token } from "../Utils/Atoms";
+import { TimeTable } from "../Pages/TimeTable";
+// import { Apply } from "../Pages/Apply";
 import { Login } from "../Pages/Login";
+import { Home } from "../Pages/Home";
+import { Dev } from "../Pages/Dev";
+import { Layout } from "./Layout";
 
 export const Router = () => {
   const setAccessToken = useSetRecoilState(token);
+  const setRefreshToken = useSetRecoilState(refresh);
   chrome.cookies.get({url: "http://localhost:3000", name:"accessToken"}).then(res => {
     res && setAccessToken(res.value);
   })
-
+  chrome.cookies.get({url: "http://localhost:3000", name:"refreshToken"}).then(res => {
+    res && setRefreshToken(res.value);
+  })
+  
   return <MemoryRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="" element={<Home />} />
         <Route path="timetable" element={<TimeTable />} />
-        <Route path="apply" element={<Apply />} />
+        <Route path="apply" element={<Dev />} />
+        <Route path="all" element={<Dev />} />
       </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
