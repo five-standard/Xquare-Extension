4d813d666c083939a3e6b0a1c2c419700f60e5d4
six-standard <dbrrl1127@gmail.com>
2023-11-03 20:03:02 +0900

    🌧 :: 홈 페이지 업데이트 로그아웃 기능 추가 및 코드 정리

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,20 +1,22 @@
-import styled from "styled-components"
-import { Box } from "../../components/Box"
-import { useEffect, useState } from "react"
-import { getTodayMeals } from "../../Api/Meal"
-import { token } from "../../Utils/Atoms"
-import { useRecoilValue } from "recoil"
-import { getUserSimple } from "../../Api/User"
-import { useNavigate } from "react-router-dom"
+/*global chrome*/
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { getTodayMeals } from "../../Api/Meal";
+import { getUserSimple } from "../../Api/User";
+import { postLogout } from "../../Api/Auth";
+import { Box } from "../../components/Box";
+import { token } from "../../Utils/Atoms";
+import * as _ from "./style";
 
 export const Home = () => {
   const [meals, setMeals] = useState({
     breakfast: undefined,
     lunch: undefined,
     dinner: undefined,
-    breakfast_Kcal: undefined,
-    lunch_Kcal: undefined,
-    dinner_Kcal: undefined
+    breakfast_kcal: undefined,
+    lunch_kcal: undefined,
+    dinner_kcal: undefined
   })
   const [user, setUser] = useState({
     name: "",
@@ -22,127 +24,93 @@ export const Home = () => {
     bad_point: 0,
     profile_file_name: ""
   })
-  const date = new Date();
+  const [accessToken, setAccessToken] = useRecoilState(token);
   const days = ["일", "월", "화", "수", "목", "금", "토"];
-  const accessToken = useRecoilValue(token);
   const navigate = useNavigate();
+  const date = new Date();
 
   useEffect(() => {
     getTodayMeals().then(res => {
       Object.keys(meals).map(key => {
-        setMeals({...meals, [key]: res.data[key]})
+        setMeals(meals => { return {...meals, [key]: res.data[key]} })
       })
     }).catch(() => {})
-    if(accessToken) {
-      getUserSimple(accessToken).then(res => {
-        Object.keys(user).map(key => {
-          setUser({...user, [key]: res.data[key]})
-        })
-      }).catch(() => {})
-    }
-  }, [])
+  })
 
-  return <Wrapper>
-    <Box height="70px" style={{"padding-right": "20px", "cursor": `${!accessToken ? "pointer" : "default"}`}} action={() => !accessToken && navigate("/login")}>
-      <ProfileBox>
+  useEffect(() => {
+    accessToken && getUserSimple(accessToken).then(res => {
+      res.data.name && Object.keys(user).map(key => {
+        setUser(user => { return {...user, [key]: res.data[key]} })
+      })
+    }).catch(() => {})
+  }, [accessToken])
+
+  const handleLogout = () => {
+    postLogout(accessToken).then(() => {
+      chrome.cookies.remove({url: "http://localhost:3000", name: "accessToken"});
+      navigate("/");
+      setAccessToken(undefined);
+    }).catch(() => {})
+  }
+
+  return <_.Wrapper>
+    <Box height="70px" style={{"padding-right": "20px", "cursor": `${!accessToken ? "pointer" : "default"}`, "justify-content": "space-between"}} action={() => !accessToken && navigate("/login")}>
+      <_.ProfileBox>
         <img src={accessToken ? user.profile_file_name : "/imgs/svg/Profile.svg"} width={40} height={40} style={{"border-radius": "50px"}}/>
         {
           accessToken
-          ? <DataBox>
+          ? <_.DataBox>
             <h1>{user.name}</h1>
             <h2>상점 {user.good_point}점 벌점 {user.bad_point}점</h2>
-          </DataBox> 
+          </_.DataBox> 
           : <h1 style={{"font-size": "20px", "color": "#5C5960"}}>로그인하세요</h1>
         }
-      </ProfileBox>
+      </_.ProfileBox>
+      { accessToken && <_.LogoutBox onClick={handleLogout}>로그아웃</_.LogoutBox>}
     </Box>
     <Box style={{"align-items": "center", "flex-direction": "column"}}>
       <h1 style={{"align-self": "flex-start"}}>{date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} ({days[date.getDay()]})</h1>
-      <MealDataBox>
-        <MealBox>
+      <_.MealDataBox>
+        <_.MealBox>
           <div id="title">
             <h1>아침</h1>
-            {meals.breakfast_Kcal && <h3>{meals.breakfast_Kcal}</h3> }
+            {meals.breakfast_kcal && <h3>{meals.breakfast_kcal}</h3> }
           </div>
-          <h2>{meals.breakfast
-          ? meals.breakfast.map((i, k) => `${i}${k !== meals.breakfast.length-1 ? ", " : ""}`)
-          : "등록된 급식이 없습니다"
-          }</h2>
-        </MealBox>
-        <MealBox>
+          <h2>
+            {
+              meals.breakfast
+              ? meals.breakfast.map((i, k) => `${i}${k !== meals.breakfast.length-1 ? ", " : ""}`)
+              : "등록된 급식이 없습니다"
+            }
+          </h2>
+        </_.MealBox>
+        <_.MealBox>
           <div id="title">
             <h1>점심</h1>
-            {meals.lunch_Kcal && <h3>{meals.lunch_Kcal}</h3> }
+            {meals.lunch_kcal && <h3>{meals.lunch_kcal}</h3> }
           </div>
-          <h2>{meals.lunch
-          ? meals.lunch.map((i, k) => `${i}${k !== meals.lunch.length-1 ? ", " : ""}`)
-          : "등록된 급식이 없습니다"
-          }</h2>
-        </MealBox>
-        <MealBox>
+          <h2>
+            {
+              meals.lunch
+              ? meals.lunch.map((i, k) => `${i}${k !== meals.lunch.length-1 ? ", " : ""}`)
+              : "등록된 급식이 없습니다"
+            }
+          </h2>
+        </_.MealBox>
+        <_.MealBox>
           <div id="title">
             <h1>저녁</h1>
-            {meals.dinner_Kcal && <h3>{meals.dinner_Kcal}</h3> }
+            {meals.dinner_kcal && <h3>{meals.dinner_kcal}</h3> }
           </div>
-          <h2>{meals.dinner
-          ? meals.dinner.map((i, k) => `${i}${k !== meals.dinner.length-1 ? ", " : ""}`)
-          : "등록된 급식이 없습니다"
-          }</h2>
-        </MealBox>
-      </MealDataBox>
+          <h2>
+            {
+              meals.dinner
+              ? meals.dinner.map((i, k) => `${i}${k !== meals.dinner.length-1 ? ", " : ""}`)
+              : "등록된 급식이 없습니다"
+            }
+          </h2>
+        </_.MealBox>
+      </_.MealDataBox>
     </Box>
-  </Wrapper>
+  </_.Wrapper>
 }
-
-const Wrapper = styled.div`
-  gap: 15px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-const DataBox = styled.div`
-  display: flex;
-  gap: 3px;
-  flex-direction: column;
-`
-
-const MealBox = styled.div`
-  gap: 5px;
-  width: 100%;
-  min-height: 85px;
-  display: flex;
-  flex-direction: column;
-  padding: 10px;
-  box-sizing: border-box;
-  background: #F9F7FA;
-  border-radius: 15px;
-  & > div#title {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-  }
-  & > h2 {
-    width: 80%;
-  }
-`
-
-const MealDataBox = styled.div`
-  width: 100%;
-  max-height: 250px;
-  border-radius: 15px;
-  display: flex;
-  flex-wrap: nowrap;
-  flex-direction: column;
-  gap: 10px;
-  overflow-y: auto;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`
-
-const ProfileBox = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 15px;
-`
---
+++ src/Pages/Home/style.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  gap: 15px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+
+export const DataBox = styled.div`
+  gap: 3px;
+  display: flex;
+  flex-direction: column;
+`
+
+export const MealBox = styled.div`
+  gap: 5px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 10px;
+  min-height: 85px;
+  background: #F9F7FA;
+  border-radius: 15px;
+  box-sizing: border-box;
+  & > div#title {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+  & > h2 { width: 80%; }
+`
+
+export const MealDataBox = styled.div`
+  gap: 10px;
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  width: 100%;
+  overflow-y: auto;
+  max-height: 250px;
+  border-radius: 15px;
+  &::-webkit-scrollbar { display: none; }
+`
+
+export const ProfileBox = styled.div`
+  gap: 15px;
+  display: flex;
+  align-items: center;
+`
+
+export const LogoutBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border-radius: 10px;
+  background: #F9F7FA;
+  box-sizing: border-box;
+  padding: 10px 15px 10px 15px;
+  color: #FF7575;
+  font-size: 12px;
+  font-weight: 500;
+`
