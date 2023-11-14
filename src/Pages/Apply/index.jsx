26e42001d87b8e8245ba642d3a8bf3285e60d516
six-standard <dbrrl1127@gmail.com>
2023-11-14 18:16:32 +0900

    🌧 :: 신청 페이지 업데이트 미완성 기능 추가

--- src/Pages/Apply/index.jsx
+++ src/Pages/Apply/index.jsx
@@ -1,16 +1,45 @@
 import styled from "styled-components"
 import { Box } from "../../components/Box"
 import { Button } from "../../Components/Button"
+import { useEffect, useState } from "react"
+import { getStayCodes, getStayStatus, postStayStatus } from "../../Api/Apply"
+import { toast } from "react-toastify"
 
 export const Apply = () => {
+  const [codes, setCodes] = useState(undefined);
+  const [state, setState] = useState(undefined);
+
+  useEffect(() => {
+    getStayCodes().then(res => {
+      setCodes(res.data.codes);
+    }).catch(() => {})
+  }, [])
+
+  useEffect(() => {
+    getStayStatus().then(res => {
+      setState(res.data.status);
+    }).catch(() => {})
+  }, [])
+
+  const handleClick = (e) => {
+    const name = e.target.className.split(" ")[2];
+    if(state !== name) {
+      setState(name);
+      postStayStatus(name).then(() => {
+        toast.success(`잔류 신청이 ${e.target.innerText}로 변경됬습니다.`)
+      }).catch(() => {})
+    }
+  }
+
   return <Wrapper>
     <Box style={{"justify-content": "space-between"}}>
       <h1>잔류 신청</h1>
       <ButtonBox>
-        <Button text="잔류" />
-        <Button text="금요귀가" id="selected" />
-        <Button text="토요귀가" />
-        <Button text="토요귀사" />
+        {
+          codes && codes.map(i => {
+            return <Button text={i.value.replaceAll(" ", "")} id={i.name === state && "selected"} className={i.name} action={handleClick}/>
+          })
+        }
       </ButtonBox>
     </Box>
     <Box style={{"justify-content": "space-between"}}>
