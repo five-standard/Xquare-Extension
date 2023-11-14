96c692af5fbf4e1f13ea495b687decc49a53ed57
six-standard <dbrrl1127@gmail.com>
2023-11-14 18:16:02 +0900

    🌧 :: 홈 페이지 업데이트

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,14 @@
-/*global chrome*/
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
 import { getTodayMeals } from "../../Api/Meal";
 import { getUserSimple } from "../../Api/User";
 import { days } from "../../Utils/DataTypes";
 import { postLogout } from "../../Api/Auth";
+import { updator } from "../../Utils/Atoms";
+import { useRecoilValue } from "recoil";
+import { Dates } from "../../Utils/Objs";
 import { Box } from "../../components/Box";
-import { token } from "../../Utils/Atoms";
+import { Cookie } from "../../Utils/Objs";
 import * as _ from "./style";
 
 export const Home = () => {
@@ -20,43 +21,41 @@ export const Home = () => {
     dinner_kcal: undefined
   })
   const [user, setUser] = useState({
-    name: "",
+    name: "아무개",
     good_point: 0,
     bad_point: 0,
-    profile_file_name: ""
+    profile_file_name: undefined
   })
-  const [accessToken, setAccessToken] = useRecoilState(token);
   const navigate = useNavigate();
-  const date = new Date();
+  const accessToken = Cookie.get("accessToken");
+  const update = useRecoilValue(updator);
 
   useEffect(() => {
-    getTodayMeals().then(res => {
-      Object.keys(meals).map(key => {
+    getTodayMeals().then(res => { // 급식정보 불러오기
+      res.data && Object.keys(meals).map(key => {
         setMeals(meals => { return {...meals, [key]: res.data[key]} })
       })
     }).catch(() => {})
-  })
-
-  useEffect(() => {
-    accessToken && getUserSimple(accessToken).then(res => {
-      Object.keys(user).map(key => {
+    accessToken && getUserSimple().then(res => { // 유저정보 불러오기
+      res.data && Object.keys(user).map(key => {
         setUser(user => { return {...user, [key]: res.data[key]} })
       })
     }).catch(() => {})
-  }, [accessToken])
+  }, [update])
 
   const handleLogout = () => {
-    postLogout(accessToken).then(() => {
-      chrome.cookies.remove({url: "http://localhost:3000", name: "accessToken"});
+    postLogout().then(() => {
+      Cookie.remove("accessToken");
+      Cookie.remove("refreshToken");
+      setUser({...user, profile_file_name: undefined});
       navigate("/");
-      setAccessToken(undefined);
     }).catch(() => {})
   }
 
   return <_.Wrapper>
     <Box height="70px" style={{"padding-right": "20px", "cursor": `${!accessToken ? "pointer" : "default"}`, "justify-content": "space-between"}} action={() => !accessToken && navigate("/login")}>
       <_.ProfileBox>
-        <img src={accessToken ? user.profile_file_name : "/imgs/svg/Profile.svg"} width={40} height={40} style={{"border-radius": "50px"}}/>
+        <img src={user.profile_file_name ? user.profile_file_name : "/imgs/svg/Profile.svg"} width={40} height={40} style={{"border-radius": "50px"}}/>
         {
           accessToken
           ? <_.DataBox>
@@ -69,7 +68,7 @@ export const Home = () => {
       { accessToken && <_.LogoutBox onClick={handleLogout}>로그아웃</_.LogoutBox>}
     </Box>
     <Box style={{"align-items": "center", "flex-direction": "column"}}>
-      <h1 style={{"align-self": "flex-start"}}>{date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} ({days[date.getDay()]})</h1>
+      <h1 style={{"align-self": "flex-start"}}>{Dates.getFullYear()}-{Dates.getMonth()+1}-{Dates.getDate()} ({days[Dates.getDay()]})</h1>
       <_.MealDataBox>
         <_.MealBox>
           <div id="title">
