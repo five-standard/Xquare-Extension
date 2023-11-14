6b78db4c7a65f53e4a64d7f58abc60b1ea914c53
six-standard <dbrrl1127@gmail.com>
2023-11-14 18:09:13 +0900

    🌧 :: 라우터 업데이트 토큰 가져오는 방식 변경

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,7 +1,4 @@
-/*global chrome*/
 import { MemoryRouter, Routes, Route } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
-import { refresh, token } from "../Utils/Atoms";
 import { TimeTable } from "../Pages/TimeTable";
 // import { Apply } from "../Pages/Apply";
 import { Login } from "../Pages/Login";
@@ -10,15 +7,6 @@ import { Dev } from "../Pages/Dev";
 import { Layout } from "./Layout";
 
 export const Router = () => {
-  const setAccessToken = useSetRecoilState(token);
-  const setRefreshToken = useSetRecoilState(refresh);
-  chrome.cookies.get({url: "http://localhost:3000", name:"accessToken"}).then(res => {
-    res && setAccessToken(res.value);
-  })
-  chrome.cookies.get({url: "http://localhost:3000", name:"refreshToken"}).then(res => {
-    res && setRefreshToken(res.value);
-  })
-  
   return <MemoryRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
