06bdc0e1b0f2f5230727273001696dd552e3dee0
six-standard <dbrrl1127@gmail.com>
2023-11-17 23:44:12 +0900

    ♻️ :: 박스 컴포넌트 리팩토링

--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -1,31 +1,33 @@
 import styled from "styled-components";
 
-export const Box = ({ children, height, style, action }) => {
-  return <Wrapper height={height} style={style} onClick={action}>
+export const Box = ({ children, height, style, action, rotate }) => {
+  return <Wrapper height={height} style={style} onClick={action} rotate={rotate}>
     {children}
   </Wrapper>
 }
 
 const Wrapper = styled.div`
+  gap: 10px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  flex-direction: ${({rotate}) => rotate ? "column" : "row"};
   width: 90%;
-  height: ${({height}) => height};
-  gap: 15px;
-  background: white;
   padding: 10px;
-  box-sizing: border-box;
   border-radius: 15px;
-  & h1 {
+  background: white;
+  box-sizing: border-box;
+  height: ${({height}) => height};
+  & h1 { // 제목
     font-size: 15px;
     color: #000000;
     font-weight: 500;
   }
-  & h2 {
+  & h2 { // 내용
     font-size: 13px;
     color: #5C5960;
   }
-  & h3 {
+  & h3 { // 내용 2
     font-size: 12px;
     color: #77757A;
   }
