399dea6cfc1532345b370ee3aee03c45b3377014
six-standard <dbrrl1127@gmail.com>
2023-11-17 23:52:20 +0900

    ♻️ :: 시간표 페이지 리팩토링

--- src/Pages/TimeTable/index.jsx
+++ src/Pages/TimeTable/index.jsx
@@ -1,50 +1,51 @@
 import { useEffect, useState } from "react";
-import { getTimeTable } from "../../Api/TimeTable";
-import { days } from "../../Utils/DataTypes";
-import { Box } from "../../components/Box";
-import { updator } from "../../Utils/Atoms";
 import { useRecoilValue } from "recoil";
-import { Dates } from "../../Utils/Objs";
-import * as _ from "./style";
 import { toast } from "react-toastify";
+import { Dates, messages, day } from "../../Utils/Utilities";
+import { getTimeTable } from "../../Api/TimeTable";
+import { MapBox } from "../../Components/MapBox";
+import { updator } from "../../Utils/Atoms";
+import { Box } from "../../components/Box";
+import * as _ from "./style";
 
 export const TimeTable = () => {
   const [timeTable, setTimeTable] = useState(undefined);
-  const today = Dates.getDay();
   const update = useRecoilValue(updator);
+  const today = Dates.getDay();
 
   useEffect(() => {
     getTimeTable().then(res => {
-      res.data.week_timetable.map((i, k) => {
-        if(i.week_day === today) {
-          setTimeTable(res.data.week_timetable[k].day_timetable);
-        }
-      })
-    }).catch(() => {})
+      const result = (res.data.week_timetable.filter(i => i.week_day === today))[0];
+      setTimeTable(result.day_timetable);
+    }).catch(() => toast.error(<b>{messages.timetable}</b>))
   }, [update])
 
+  const handleOpen = () => {
+    window.open("https://dsmhs.djsch.kr/scheduleH/list.do?m=0203&s=dsmhs");
+  }
+
   return <_.Wrapper>
-    <Box style={{"align-items": "center", "flex-direction": "column"}}>
-      <h1 style={{"align-self": "flex-start"}}>시간표 ({days[today]})</h1>
+    <Box rotate>
+      <h1 style={{"align-self": "flex-start"}}>시간표 ({day})</h1>
       <_.SubjDataBox>
         {
           timeTable
           ? timeTable.map((i, k) => {
             const begin = i.begin_time.split(":");
             const end = i.end_time.split(":");
-            return <_.SubjectBox>
+            return <MapBox>
               <div id="class" key={k}>
                 <h1>{i.subject_name}</h1>
                 <h2>{begin[0]}:{begin[1]} ~ {end[0]}:{end[1]}</h2>
               </div>
-              <img src={i.subject_image} alt="" width={40} height={40} />
-            </_.SubjectBox>
+              <img src={i.subject_image} width={40} height={40} />
+            </MapBox>
           })
           : <h1>등록된 수업이 없습니다.</h1>
         }
       </_.SubjDataBox>
     </Box>
-    <Box height="70px" style={{"justify-content": "space-between", "cursor": "pointer", "padding-right": "20px", "padding-left": "20px"}} action={() => window.open("https://dsmhs.djsch.kr/scheduleH/list.do?m=0203&s=dsmhs")}>
+    <Box height="70px" style={{"cursor": "pointer", "padding": "0 20px 0 20px"}} action={handleOpen}>
       <h1 style={{"font-size": "20px", "color": "#5C5960"}}>학사일정</h1>
       <img src="/imgs/svg/Url.svg" width={20} height={20} />
     </Box>
--- src/Pages/TimeTable/style.js
+++ src/Pages/TimeTable/style.js
@@ -7,26 +7,6 @@ export const Wrapper = styled.div`
   flex-direction: column;
 `
 
-export const SubjectBox = styled.div`
-  gap: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  padding: 10px;
-  min-height: 60px;
-  background: #F9F7FA;
-  border-radius: 15px;
-  box-sizing: border-box;
-  & > div#class {
-    gap: 2px;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-  }
-  & > h2 { width: 30%; }
-`
-
 export const SubjDataBox = styled.div`
   gap: 10px;
   display: flex;
