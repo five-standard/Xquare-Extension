d12da1d36e7242fc6861647c9269a387ff869f83
six-standard <dbrrl1127@gmail.com>
2023-11-17 23:44:34 +0900

    ♻️ :: 입력 컴포넌트 리팩토링

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -9,10 +9,8 @@ const Content = styled.input`
   height: 40px;
   padding: 10px;
   box-sizing: border-box;
-  background: ${({value}) => value !== "" ? "#FFFFFF": "#bdbbbb"};
-  border: 1px solid ${({value}) => value !== "" ? "#000000" : "#635d63"};
+  background: ${({value}) => value !== "" ? "#eeeeee": "#dadada"};
+  border: 1px solid ${({value}) => value !== "" ? "#9e9e9e" : "#aaaaaa"};
   border-radius: 15px;
-  &::placeholder {
-    color: #605f66;
-  }
+  &::placeholder { color: #605f66; }
 `
