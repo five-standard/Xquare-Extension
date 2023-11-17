5f04ebba5c706c689d428d3f9347156362dcf10a
six-standard <dbrrl1127@gmail.com>
2023-11-17 23:49:48 +0900

    ♻️ :: 홈 페이지 리팩토링

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,14 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { toast } from "react-toastify";
+import { Cookie, Dates, messages, days } from "../../Utils/Utilities";
+import { MapBox } from "../../Components/MapBox";
 import { getTodayMeals } from "../../Api/Meal";
 import { getUserSimple } from "../../Api/User";
-import { days } from "../../Utils/DataTypes";
 import { postLogout } from "../../Api/Auth";
 import { updator } from "../../Utils/Atoms";
-import { useRecoilValue } from "recoil";
-import { Dates } from "../../Utils/Objs";
 import { Box } from "../../components/Box";
-import { Cookie } from "../../Utils/Objs";
 import * as _ from "./style";
 
 export const Home = () => {
@@ -26,36 +26,41 @@ export const Home = () => {
     bad_point: 0,
     profile_file_name: undefined
   })
-  const navigate = useNavigate();
   const accessToken = Cookie.get("accessToken");
   const update = useRecoilValue(updator);
+  const navigate = useNavigate();
+  const today = `${Dates.getFullYear()}-${Dates.getMonth()+1}-${Dates.getDate()}`
+  const mealType = {
+    "아침": "breakfast",
+    "점심": "lunch",
+    "저녁": "dinner"
+  }
 
   useEffect(() => {
     getTodayMeals().then(res => { // 급식정보 불러오기
-      res.data && Object.keys(meals).map(key => {
+      Object.keys(meals).map(key => {
         setMeals(meals => { return {...meals, [key]: res.data[key]} })
       })
-    }).catch(() => {})
+    }).catch(() => toast.error(<b>{messages.meal}</b>))
     accessToken && getUserSimple().then(res => { // 유저정보 불러오기
       res.data && Object.keys(user).map(key => {
         setUser(user => { return {...user, [key]: res.data[key]} })
       })
-    }).catch(() => {})
+    }).catch(() => toast.error(<b>{messages.user}</b>))
   }, [update])
 
   const handleLogout = () => {
     postLogout().then(() => {
       Cookie.remove("accessToken");
       Cookie.remove("refreshToken");
-      setUser({...user, profile_file_name: undefined});
       navigate("/");
-    }).catch(() => {})
+    }).catch(() => toast.error(<b>{messages.logout}</b>))
   }
 
   return <_.Wrapper>
-    <Box height="70px" style={{"padding-right": "20px", "cursor": `${!accessToken ? "pointer" : "default"}`, "justify-content": "space-between"}} action={() => !accessToken && navigate("/login")}>
+    <Box height="70px" style={{"padding-right": "20px", "cursor": `${!accessToken ? "pointer" : "default"}`}} action={() => !accessToken && navigate("/login")}>
       <_.ProfileBox>
-        <img src={user.profile_file_name ? user.profile_file_name : "/imgs/svg/Profile.svg"} width={40} height={40} style={{"border-radius": "50px"}}/>
+        <img src={accessToken ? user.profile_file_name : "/imgs/svg/Profile.svg"} width={40} height={40} style={{"border-radius": "50px"}}/>
         {
           accessToken
           ? <_.DataBox>
@@ -67,48 +72,26 @@ export const Home = () => {
       </_.ProfileBox>
       { accessToken && <_.LogoutBox onClick={handleLogout}>로그아웃</_.LogoutBox>}
     </Box>
-    <Box style={{"align-items": "center", "flex-direction": "column"}}>
-      <h1 style={{"align-self": "flex-start"}}>{Dates.getFullYear()}-{Dates.getMonth()+1}-{Dates.getDate()} ({days[Dates.getDay()]})</h1>
+    <Box style={{"flex-direction": "column"}}>
+      <h1 style={{"align-self": "flex-start"}}>{today} ({days[Dates.getDay()]})</h1>
       <_.MealDataBox>
-        <_.MealBox>
-          <div id="title">
-            <h1>아침</h1>
-            {meals.breakfast_kcal && <h3>{meals.breakfast_kcal}</h3> }
-          </div>
-          <h2>
-            {
-              meals.breakfast
-              ? meals.breakfast.map((i, k) => `${i}${k !== meals.breakfast.length-1 ? ", " : ""}`)
-              : "등록된 급식이 없습니다"
-            }
-          </h2>
-        </_.MealBox>
-        <_.MealBox>
-          <div id="title">
-            <h1>점심</h1>
-            {meals.lunch_kcal && <h3>{meals.lunch_kcal}</h3> }
-          </div>
-          <h2>
-            {
-              meals.lunch
-              ? meals.lunch.map((i, k) => `${i}${k !== meals.lunch.length-1 ? ", " : ""}`)
-              : "등록된 급식이 없습니다"
-            }
-          </h2>
-        </_.MealBox>
-        <_.MealBox>
-          <div id="title">
-            <h1>저녁</h1>
-            {meals.dinner_kcal && <h3>{meals.dinner_kcal}</h3> }
-          </div>
-          <h2>
-            {
-              meals.dinner
-              ? meals.dinner.map((i, k) => `${i}${k !== meals.dinner.length-1 ? ", " : ""}`)
-              : "등록된 급식이 없습니다"
-            }
-          </h2>
-        </_.MealBox>
+        {
+          Object.entries(mealType).map(([k, v]) => {
+            return <MapBox style={{"flex-direction": "column"}}>
+              <div id="title">
+                <h1>{k}</h1>
+                {meals[`${v}_kcal`] && <h3>{meals[`${v}_kcal`]}</h3> }
+              </div>
+              <h2>
+              {
+                meals[v]
+                ? meals[v].map((i, k) => `${i}${k !== meals[v].length-1 ? ", " : ""}`)
+                : "등록된 급식이 없습니다"
+              }
+              </h2>
+            </MapBox>
+          })
+        }
       </_.MealDataBox>
     </Box>
   </_.Wrapper>
