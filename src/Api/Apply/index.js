6a982289b07c12843415dbfe954b61097936b685
six-standard <dbrrl1127@gmail.com>
2023-11-22 14:31:16 +0900

    🐛 :: 신청 페이지 오류 수정 요청이 전송되지 않는 오류 수정

--- src/Api/Apply/index.js
+++ src/Api/Apply/index.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { instance } from "../axios"
 
 export const getStayCodes = async () => { // 잔류 항목 불러오기
@@ -10,5 +9,5 @@ export const getStayStatus = async () => { // 잔류 정보 불러오기
 }
 
 export const postStayStatus = async (state) => { // 잔류 상태 변경 요청 보내기
-  return await axios.put(`${process.env.REACT_APP_API_KEY}/applications/stay`, { status: state });
+  return await instance.put(`/applications/stay`, { status: state });
 }
