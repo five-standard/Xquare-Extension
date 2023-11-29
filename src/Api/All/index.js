641c12e025b345a285bd1dc16c41630c67b23b35
six-standard <dbrrl1127@gmail.com>
2023-11-29 21:54:03 +0900

    🌧 :: 전체 페이지 업데이트 자습감독 선생님 목록 추가

--- src/Api/All/index.js
+++ src/Api/All/index.js
@@ -1,5 +1,6 @@
+import { Dates } from "../../Utils/Utilities";
 import { instance } from "../axios"
 
-export const getStayCodes = async () => { // 자습감독 선생님 불러오기
-  return await instance.get("/admin/director");
+export const getDirectors = async () => { // 자습감독 선생님 불러오기
+  return await instance.get(`/pick/admin/director?month=${Dates.getMonth()+1}`);
 }
--- src/Pages/All/index.jsx
+++ src/Pages/All/index.jsx
@@ -1,12 +1,22 @@
 import { styled } from "styled-components";
 import { Box } from "../../components/Box";
 import { MapBox } from "../../Components/MapBox";
-import { day } from "../../Utils/Utilities";
+import { Dates, day, messages } from "../../Utils/Utilities";
 import { today } from "../../Utils/Utilities";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getDirectors, getNotices } from "../../Api/All";
+import { toast } from "react-toastify";
 
 export const All = () => {
   const navigate = useNavigate();
+  const [director, setDirector] = useState(undefined);
+
+  useEffect(() => {
+    getDirectors().then(res => {
+      res.data && setDirector(res.data.self_study_list[Dates.getDate()-1].teacher);
+    }).catch(() => toast.error(<b>{messages.director}</b>))
+  }, [])
 
   const handleClick = (e) => {
     navigate(e.target.id);
@@ -15,15 +25,17 @@ export const All = () => {
   return <Wrapper>
     <Row>
       <div>
-        <Box height="95px" style={{"width": "100%"}} action={handleClick} id="/points"><h1 style={{"font-size": "20px", "color": "#5C5960", "cursor": "pointer"}}>상벌점 내역</h1></Box>
-        <Box height="95px" style={{"width": "100%"}} action={handleClick} id="/home"><h1 style={{"font-size": "20px", "color": "#5C5960", "cursor": "pointer"}}>개발중...</h1></Box>
+        <Box style={{"width": "100%", "height": "100%"}} action={handleClick} id="/points" cursor><h1 style={{"font-size": "20px", "color": "#5C5960"}}>상벌점 내역</h1></Box>
+        <Box style={{"width": "100%", "height": "100%"}} action={handleClick} id="/all" cursor><h1 style={{"font-size": "20px", "color": "#5C5960"}}>개발중...</h1></Box>
       </div>
       <div>
         <Box style={{"width": "100%", "min-height": "200px", "justify-content": "flex-start"}} rotate>
           <h1 style={{"align-self": "flex-start"}}>{day}요일 자습감독</h1>
-          <MapBox style={{"height": "45px"}}><h1>2층</h1><h2>정은진 선생님</h2></MapBox>
-          <MapBox style={{"height": "45px"}}><h1>3층</h1><h2>정은진 선생님</h2></MapBox>
-          <MapBox style={{"height": "45px"}}><h1>4층</h1><h2>정은진 선생님</h2></MapBox>
+          {
+            director && director.map((i, j) => {
+              if(i !== "") return <MapBox style={{"height": "45px", "justify-content": "space-between"}}><h1>{j+1}층</h1><h2>{i} 선생님</h2></MapBox>
+            })
+          }
         </Box>
       </div>
     </Row>
@@ -33,17 +45,20 @@ export const All = () => {
 const Wrapper = styled.div`
   gap: 10px;
   display: flex;
+  align-items: center;
   flex-direction: column;
 `
 
 const Row = styled.div`
-  width: 100%;
+  width: 90%;
   gap: 10px;
   display: flex;
+  justify-content: center;
   & > div {
     gap: 10px;
     display: flex;
     flex-direction: column;
-    width: 60%;
+    width: 100%;
+    min-height: 200px;
   }
 `
