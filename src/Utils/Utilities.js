9486586387a71d649bdeae3c90b2191ba1daf1ea
six-standard <dbrrl1127@gmail.com>
2023-11-29 23:02:50 +0900

    🌧 :: 자습감독 선생님 메시지 추가

--- src/Utils/Utilities.js
+++ src/Utils/Utilities.js
@@ -8,11 +8,12 @@ export const days = ["일", "월", "화", "수", "목", "금", "토"];
 
 export const day = days[Dates.getDay()];
 
-export const today = `${Dates.getFullYear()}-${Dates.getMonth() - 1}-${Dates.getDate()}`
+export const today = `${Dates.getFullYear()}-${Dates.getMonth() + 1}-${Dates.getDate()}`
 
 export const messages = {
   user: "유저 정보를 불러오는 데 실패했습니다.",
   meal: "급식 정보를 불러오는 데 실패했습니다.",
+  director: "자습감독 선생님 목록을 불러오는 데 실패했습니다.",
   timetable: "시간표를 불러오는 데 실패했습니다.",
   logout: "로그아웃하는 데 실패했습니다.",
   login_success: "로그인되었습니다.",
