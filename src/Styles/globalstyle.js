3ea98573fed4ca4ddd690c4c9e495fd053a70d05
six-standard <dbrrl1127@gmail.com>
2023-12-03 01:47:43 +0900

    🌧 :: globalStyle 수정 h1, h2, h3 추가

--- src/Styles/globalstyle.js
+++ src/Styles/globalstyle.js
@@ -9,4 +9,20 @@ export const GlobalStyle = createGlobalStyle`
     font-weight: 400;
     font-family: "Noto Sans KR";
   }
+
+  h1 { // 제목
+    font-size: 15px;
+    color: #000000;
+    font-weight: 500;
+  }
+
+  h2 { // 내용
+    font-size: 13px;
+    color: #5C5960;
+  }
+  
+  h3 { // 내용 2
+    font-size: 12px;
+    color: #77757A;
+  }
 `;
