8615376d506682d7c57a9d63c7b3d79e91a9b8ec
six-standard <dbrrl1127@gmail.com>
2023-12-03 01:45:11 +0900

    🐛 :: 공지사항 API 오류 수정 API가 불러와지지 않던 오류 수정

--- src/Api/All/index.js
+++ src/Api/All/index.js
@@ -5,6 +5,6 @@ export const getDirectors = async () => { // 자습감독 선생님 불러오기
   return await instance.get(`/pick/admin/director?month=${Dates.getMonth()+1}`);
 }
 
-export const getNotices = async () => {
-  return await instance.get("/feeds");
+export const getNotices = async () => { // 공지사항 불러오기
+  return await instance.get("/feeds/?category=38313230-3434-3839-2d62-6133372d3131");
 }
