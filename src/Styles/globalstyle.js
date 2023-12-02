49912a9d3df6dffc2c364cb40d9d5ff63ae79d77
six-standard <dbrrl1127@gmail.com>
2023-12-03 03:06:14 +0900

    ♻️ :: globalStyle 줄넘김 추가

--- src/Styles/globalstyle.js
+++ src/Styles/globalstyle.js
@@ -6,7 +6,7 @@ export const GlobalStyle = createGlobalStyle`
     border: none;
     outline: none;
     user-select: none;
-    font-weight: 400;
+    font-weight: 100;
     font-family: "Noto Sans KR";
   }
 
@@ -20,7 +20,7 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 13px;
     color: #5C5960;
   }
-  
+
   h3 { // 내용 2
     font-size: 12px;
     color: #77757A;
