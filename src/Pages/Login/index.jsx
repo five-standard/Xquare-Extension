07305d97368e13f9d89af559f144ca74fd872f59
six-standard <dbrrl1127@gmail.com>
2023-12-03 15:15:53 +0900

    ♻️ :: 로그인 페이지 코드 정리

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -21,22 +21,20 @@ export const Login = () => {
   }
 
   const handleSubmit = () => {
-    postLogin(data).then(res => {
+    postLogin(data).then(res => { // 로그인 요청하기
       Cookie.set("accessToken", res.data.access_token);
       Cookie.set("refreshToken", res.data.refresh_token);
       navigate("/");
       toast.success(<b>{messages.login_success} ({data.account_id})</b>);
-    }).catch(e => {
-      toast.error(<b>{messages.login_fail} ({e.response.data.error_message.split(".")[0]})</b>);
-    })
+    }).catch(e => toast.error(<b>{messages.login_fail} ({e.response.data.error_message.split(".")[0]})</b>))
   }
 
   return <_.Wrapper>
-    <Back to="/" />
+    <Back />
     <_.InputBox>
       <Input placeholder="아이디" value={data.account_id} action={handleChange} id="account_id" />
       <Input placeholder="비밀번호" value={data.password} action={handleChange} id="password" type="password" />
-      <Button action={handleSubmit} text="로그인" style={{"width": "70%", "height": "50px", "font-size": "20px"}} id="selected" />
+      <Button action={handleSubmit} text="로그인" style={{width: "70%", height: "50px", fontSize: "20px"}} id="selected" />
     </_.InputBox>
   </_.Wrapper>
 }
