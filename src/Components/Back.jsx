316c3ca887049be53a6352847db79f1ffc4a7f49
six-standard <dbrrl1127@gmail.com>
2023-12-03 15:11:27 +0900

    🌧 :: Back 컴포넌트 수정 URL 고정

--- src/Components/Back.jsx
+++ src/Components/Back.jsx
@@ -2,8 +2,8 @@ import { styled } from "styled-components";
 import { Link } from "react-router-dom";
 import { Icon as IconElement } from "@iconify/react";
 
-export const Back = ({ to }) => {
-  return <Component to={to}>
+export const Back = () => {
+  return <Component to="/">
     <IconElement icon="ph:arrow-up-bold" color="#5C5960" rotate="270deg" width="20px" />
   </Component>
 }
