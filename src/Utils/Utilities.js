f22fafe2d7cbc28edb25db0dde23ce96b9672b5b
six-standard <dbrrl1127@gmail.com>
2023-12-08 23:37:50 +0900

    🌧 :: Utilities 수정 필요한 값 추가

--- src/Utils/Utilities.js
+++ src/Utils/Utilities.js
@@ -6,6 +6,10 @@ export const day = dayType[Dates.getDay()];
 
 export const today = `${Dates.getFullYear()}-${String(Dates.getMonth() + 1).padStart(2, "0")}-${String(Dates.getDate()).padStart(2, "0")}`
 
+export const year_month = (date) => { return `${Dates.getFullYear()}-${String(Dates.getMonth() + 1).padStart(2, "0")}-${String(date).padStart(2, "0")}`; }
+
+export const lastDay = `${new Date(Dates.getFullYear(), Dates.getMonth()+1, 0).getDate()}`;
+
 export const messages = {
   user: "유저 정보를 불러오는 데 실패했습니다.",
   meal: "급식 정보를 불러오는 데 실패했습니다.",
@@ -18,5 +22,6 @@ export const messages = {
   stay_status: "잔류 상태를 불러오는 데 실패했습니다.",
   stay_status_change: "잔류 상태를 변경하는 데 실패했습니다.",
   feeds: "최근 공지사항을 불러오는 데 실패했습니다.",
+  points: "상/벌점을 불러오는 데 실패했습니다.",
   error: "다시 시도해 주세요.",
 }
