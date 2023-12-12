c583912da8e7981996c06dcec1e3723958399508
six-standard <dbrrl1127@gmail.com>
2023-12-12 18:20:06 +0900

    ♻️ :: globalFont 병합 globalStyle과 globalFront 병합

--- src/Styles/globalstyle.js
+++ src/Styles/globalstyle.js
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from "styled-components";
+import Font from "../Assets/fonts/NotoSansKR.ttf";
 
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Noto Sans KR';
+    src: url(${Font});
+    font-style: 500;
+  }
+
   * {
     margin: 0;
     border: none;
