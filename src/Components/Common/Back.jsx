0f6d9ebe8daff8f0195bcf7f0f66cf21875e8285
six-standard <dbrrl1127@gmail.com>
2023-12-17 23:16:53 +0900

    🐛 :: Back 컴포넌트 오류 수정 패딩이 이상하게 적용되던 오류 수정

--- src/Components/Common/Back.jsx
+++ src/Components/Common/Back.jsx
@@ -3,15 +3,16 @@ import { Link } from "react-router-dom";
 import { Icon as IconElement } from "@iconify/react";
 
 export const Back = () => {
-  return <Component to="/">
-    <IconElement icon="ph:arrow-up-bold" color="#5C5960" rotate="270deg" width="20px" />
+  return <Component>
+    <Link to="/">
+      <IconElement icon="ph:arrow-up-bold" color="#5C5960" rotate="270deg" width="20px" />
+    </Link>
   </Component>
 }
 
-const Component = styled(Link)`
-  align-self: start;
-  justify-self: start;
+const Component = styled.div`
+  width: 100%;
+  padding: 5.25px;
   cursor: pointer;
-  padding: 5px;
   box-sizing: border-box;
 `
