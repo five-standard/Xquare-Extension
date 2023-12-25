939542cda5de628d2b886c02f1ba5b3164454fdb
six-standard <dbrrl1127@gmail.com>
2023-12-25 23:34:54 +0900

    ♻️ :: 상벌점 페이지 스타일 리팩토링 쓸모없는 코드 제거

--- src/Pages/Points/style.js
+++ src/Pages/Points/style.js
@@ -1,14 +1,5 @@
 import { styled } from "styled-components";
 
-export const Wrapper = styled.div`
-  gap: 10px;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  height: calc(100% - 15px);
-`
-
 export const ButtonBox = styled.div`
   gap: 5px;
   display: flex;
