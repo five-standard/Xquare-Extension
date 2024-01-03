5a761b7dd38e8410af584887d6d70cdbe0060318
six-standard <dbrrl1127@gmail.com>
2024-01-03 22:58:27 +0900

    🌧 :: 오류 해결 버튼 추가

--- src/Pages/All/index.jsx
+++ src/Pages/All/index.jsx
@@ -2,20 +2,42 @@ import { PageButton } from "../../Components/Common/PageButton";
 import { NoticeBox } from "../../Components/All/NoticeBox";
 import { DirectBox } from "../../Components/All/DirectBox";
 import * as _ from "./style";
+import { toast } from "react-toastify";
 
 export const All = () => {
-  return <>
-    <_.PageButtonBox>
-      <PageButton to="/points" text="상벌점 내역" icon="mdi:like-outline" />
-      <PageButton to="/updates" text="업데이트 내역" icon="ic:baseline-update" />
-    </_.PageButtonBox>
-    <_.PageButtonBox>
-      <PageButton to="/profile" text="내 프로필" icon="gg:profile" />
-      <PageButton to="/notices" text="최근 공지사항" icon="ic:outline-announcement" />
-    </_.PageButtonBox>
-    <_.PageButtonBox>
-      <NoticeBox />
-      <DirectBox />
-    </_.PageButtonBox>
-  </>
-}
+  const handleClick = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    toast.success("완료되었습니다. 앱을 재시작해주세요");
+  };
+
+  return (
+    <>
+      <_.PageButtonBox>
+        <PageButton to="/points" text="상벌점 내역" icon="mdi:like-outline" />
+        <PageButton
+          to="/updates"
+          text="업데이트 내역"
+          icon="ic:baseline-update"
+        />
+      </_.PageButtonBox>
+      <_.PageButtonBox>
+        <PageButton to="/profile" text="내 프로필" icon="gg:profile" />
+        <PageButton
+          to="/notices"
+          text="최근 공지사항"
+          icon="ic:outline-announcement"
+        />
+      </_.PageButtonBox>
+      <_.PageButtonBox>
+        <NoticeBox />
+        <_.ColumnBox>
+          <_.ErrorButton onClick={handleClick}>
+            <_.Text>오류 해결하기</_.Text>
+          </_.ErrorButton>
+          <DirectBox />
+        </_.ColumnBox>
+      </_.PageButtonBox>
+    </>
+  );
+};
--- src/Pages/All/style.js
+++ src/Pages/All/style.js
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   align-items: center;
   flex-direction: column;
   height: calc(100% - 40px);
-`
+`;
 
 export const RowBox = styled.div`
   gap: 10px;
@@ -19,10 +19,32 @@ export const RowBox = styled.div`
     flex-direction: column;
     width: 100%;
   }
-`
+`;
 
 export const PageButtonBox = styled.div`
   width: 90%;
   display: flex;
   justify-content: space-between;
-`
+`;
+
+export const ColumnBox = styled.div`
+  gap: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ErrorButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 196px;
+  height: 50px;
+  background: #ffffff;
+  border-radius: 20px;
+  text-decoration-line: none;
+`;
+
+export const Text = styled.h1`
+  font-size: 15px;
+  color: #5c5960;
+`;
